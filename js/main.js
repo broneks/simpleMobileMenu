02c3ecb633fd6eb397e2fa700c707a8f033f678f
Bszulc <bronekszulc@gmail.com>
2014-06-21 11:02:32 -0400

    fix

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-	var breakpoint = 768;
-	var $window = $(window);
-	var $navul  = $('nav ul');
+	var breakpoint  = 768;
+	var $window     = $(window);
+	var $navul      = $('nav ul');
 	var $navtrigger = $('#mobile-nav-trigger');
 
 	$navtrigger.on('click', function() { 
 		var $that = $(this);
 
-		$navul.stop().slideToggle(function() {
-			$that.toggleClass('active');
+		$that.prop('disabled', true);
+
+		$navul.slideToggle(function() {
+			$that.toggleClass('active').prop('disabled', false);
 		});
 	});
 
@@ -20,13 +22,12 @@ $(document).ready(function() {
 	$window.on('resize', function() {
 		if ($window.width() <= breakpoint) {
 			if ($navtrigger.is(':hidden')) {
-				$navtrigger.removeClass('active');
-				$navtrigger.show();
+				$navtrigger.removeClass('active').show();
 				$navul.hide();
 			}
 		} else {
 			if ($navtrigger.is(':visible')) {
-				$navtrigger.hide();
+				$navtrigger.hide().removeClass('active');
 				$navul.show();
 			}
 		}
